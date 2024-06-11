0662f00fbf287a20400aa2c4ab411860b0d9d579
Toby <tobyeclark@gmail.com>
2024-06-11 16:46:16 +0100

    Added move logic for rook

--- Rook.java
+++ Rook.java
@@ -13,6 +13,38 @@ public class Rook extends Piece{
     @Override
     public ArrayList<Coordinate> getPossibleMoves(Board board) {
         ArrayList<Coordinate> moves = new ArrayList<>();
+        for(int x = this.x+1; x < 8 && x >= 0; x++){
+            if(board.getPiece(x, y) != null){
+                if(board.getPiece(x, y).getDirection() != direction)
+                    moves.add(new Coordinate(x, y));
+                break;
+            }
+            moves.add(new Coordinate(x, y));
+        }
+        for(int x = this.x-1; x < 8 && x >= 0; x--){
+            if(board.getPiece(x, y) != null){
+                if(board.getPiece(x, y).getDirection() != direction)
+                    moves.add(new Coordinate(x, y));
+                break;
+            }
+            moves.add(new Coordinate(x, y));
+        }
+        for(int y = this.y+1; y < 8 && y >= 0; y++){
+            if(board.getPiece(x, y) != null){
+                if(board.getPiece(x, y).getDirection() != direction)
+                    moves.add(new Coordinate(x, y));
+                break;
+            }
+            moves.add(new Coordinate(x, y));
+        }
+        for(int y = this.y-1; y < 8 && y >= 0; y--){
+            if(board.getPiece(x, y) != null){
+                if(board.getPiece(x, y).getDirection() != direction)
+                    moves.add(new Coordinate(x, y));
+                break;
+            }
+            moves.add(new Coordinate(x, y));
+        }
         return moves;
     }
 
