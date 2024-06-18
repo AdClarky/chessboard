604c1245ffd4f9ae5b5b84dc94da853bd45680a3
Toby <tobyeclark@gmail.com>
2024-06-18 17:12:47 +0100

    Fixed issue with converting coordinates when a piece was in check/checkmated

--- chessboard/Coordinate.java
+++ chessboard/Coordinate.java
@@ -11,8 +11,11 @@ public record Coordinate(int x, int y) {
     }
 
     public static Coordinate fromString(CharSequence move){
-        int x = Math.abs(7 - (move.charAt(move.length() - 2) - 97));
-        int y = move.charAt(move.length() - 1) - '0' - 1;
+        int length = move.length();
+        if(move.charAt(length - 1) == '+' || move.charAt(length - 1) == '#')
+            length--; // so it ignores the checks/checkmates
+        int x = Math.abs(7 - (move.charAt(length - 2) - 97));
+        int y = move.charAt(length - 1) - '0' - 1;
         return new Coordinate(x, y);
     }
 }
