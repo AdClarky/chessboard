2d85b29bfb7bd3973353d3326249092b92f839f6
Toby <tobyeclark@gmail.com>
2024-06-27 19:08:19 +0100

    Updated utils test to work with new chessboard

--- src/test/java/ChessUtilsTest.java
+++ src/test/java/ChessUtilsTest.java
@@ -24,64 +24,69 @@ class ChessUtilsTest {
 
     @Test
     void dRookDisambiguated(){
-//        Rook dRook = new Rook(4,7, Piece.BLACK_PIECE);
-//        Rook hRook = new Rook(0,7, Piece.BLACK_PIECE);
-//        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
-//        possiblePieces.add(dRook);
-//        possiblePieces.add(hRook);
-//        ChessUtils.disambiguatePiece(possiblePieces, "Rdf8");
-//        assertEquals(1, possiblePieces.size());
-//        assertEquals(dRook, possiblePieces.getFirst());
+        Chessboard board = new Chessboard();
+        Rook dRook = new Rook(4,7, Piece.BLACK_PIECE, board);
+        Rook hRook = new Rook(0,7, Piece.BLACK_PIECE, board);
+        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
+        possiblePieces.add(dRook);
+        possiblePieces.add(hRook);
+        ChessUtils.disambiguatePiece(possiblePieces, "Rdf8");
+        assertEquals(1, possiblePieces.size());
+        assertEquals(dRook, possiblePieces.getFirst());
     }
 
     @Test
     void hRookDisambiguated(){
-//        Rook dRook = new Rook(4,7, Piece.BLACK_PIECE);
-//        Rook hRook = new Rook(0,7, Piece.BLACK_PIECE);
-//        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
-//        possiblePieces.add(dRook);
-//        possiblePieces.add(hRook);
-//        ChessUtils.disambiguatePiece(possiblePieces, "Rhf8");
-//        assertEquals(1, possiblePieces.size());
-//        assertEquals(hRook, possiblePieces.getFirst());
+        Chessboard board = new Chessboard();
+        Rook dRook = new Rook(4,7, Piece.BLACK_PIECE, board);
+        Rook hRook = new Rook(0,7, Piece.BLACK_PIECE, board);
+        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
+        possiblePieces.add(dRook);
+        possiblePieces.add(hRook);
+        ChessUtils.disambiguatePiece(possiblePieces, "Rhf8");
+        assertEquals(1, possiblePieces.size());
+        assertEquals(hRook, possiblePieces.getFirst());
     }
 
     @Test
     void RookA5Disambiguated() {
-//        Rook a1Rook = new Rook(7, 0, Piece.BLACK_PIECE);
-//        Rook a5hRook = new Rook(7, 4, Piece.BLACK_PIECE);
-//        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
-//        possiblePieces.add(a1Rook);
-//        possiblePieces.add(a5hRook);
-//        ChessUtils.disambiguatePiece(possiblePieces, "R5a3");
-//        assertEquals(1, possiblePieces.size());
-//        assertEquals(a5hRook, possiblePieces.getFirst());
+        Chessboard board = new Chessboard();
+        Rook a1Rook = new Rook(7, 0, Piece.BLACK_PIECE, board);
+        Rook a5hRook = new Rook(7, 4, Piece.BLACK_PIECE, board);
+        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
+        possiblePieces.add(a1Rook);
+        possiblePieces.add(a5hRook);
+        ChessUtils.disambiguatePiece(possiblePieces, "R5a3");
+        assertEquals(1, possiblePieces.size());
+        assertEquals(a5hRook, possiblePieces.getFirst());
     }
 
     @Test
     void RookA1Disambiguated() {
-//        Rook a1Rook = new Rook(7, 0, Piece.BLACK_PIECE);
-//        Rook a5hRook = new Rook(7, 4, Piece.BLACK_PIECE);
-//        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
-//        possiblePieces.add(a1Rook);
-//        possiblePieces.add(a5hRook);
-//        ChessUtils.disambiguatePiece(possiblePieces, "R1a3");
-//        assertEquals(1, possiblePieces.size());
-//        assertEquals(a1Rook, possiblePieces.getFirst());
+        Chessboard board = new Chessboard();
+        Rook a1Rook = new Rook(7, 0, Piece.BLACK_PIECE, board);
+        Rook a5hRook = new Rook(7, 4, Piece.BLACK_PIECE, board);
+        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
+        possiblePieces.add(a1Rook);
+        possiblePieces.add(a5hRook);
+        ChessUtils.disambiguatePiece(possiblePieces, "R1a3");
+        assertEquals(1, possiblePieces.size());
+        assertEquals(a1Rook, possiblePieces.getFirst());
     }
 
     @Test
     void queenDoubleDisambiguation(){
-//        Queen queenE4 = new Queen(3,3, Piece.BLACK_PIECE);
-//        Queen queenH4 = new Queen(0,3, Piece.BLACK_PIECE);
-//        Queen queenH1 = new Queen(0,0, Piece.BLACK_PIECE);
-//        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
-//        possiblePieces.add(queenH1);
-//        possiblePieces.add(queenH4);
-//        possiblePieces.add(queenE4);
-//        ChessUtils.disambiguatePiece(possiblePieces, "Qh4e1");
-//        assertEquals(1, possiblePieces.size());
-//        assertEquals(queenH4, possiblePieces.getFirst());
+        Chessboard board = new Chessboard();
+        Queen queenE4 = new Queen(3,3, Piece.BLACK_PIECE, board);
+        Queen queenH4 = new Queen(0,3, Piece.BLACK_PIECE, board);
+        Queen queenH1 = new Queen(0,0, Piece.BLACK_PIECE, board);
+        SequencedCollection<Piece> possiblePieces = new ArrayList<>(2);
+        possiblePieces.add(queenH1);
+        possiblePieces.add(queenH4);
+        possiblePieces.add(queenE4);
+        ChessUtils.disambiguatePiece(possiblePieces, "Qh4e1");
+        assertEquals(1, possiblePieces.size());
+        assertEquals(queenH4, possiblePieces.getFirst());
     }
 
     @Disabled
