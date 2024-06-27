6e33d1b2ca2f5b1d747c2b35efb89b081a8fd2dd
Toby <tobyeclark@gmail.com>
2024-06-27 19:35:52 +0100

    Updated tests to work with new classes

--- src/test/java/RookTest.java
+++ src/test/java/RookTest.java
@@ -1,28 +1,31 @@
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//
-//public class RookTest {
-//    @Test
-//    void rookHasntMoved(){
-//        Rook rook = new Rook(1, 1, Piece.WHITE_PIECE);
-//        Rook rook2 = new Rook(1, 1, Piece.WHITE_PIECE);
-//        assertEquals(rook, rook2);
-//    }
-//
-//    @Test
-//    void oneRookMoved(){
-//        Rook rook = new Rook(1, 1, Piece.WHITE_PIECE);
-//        rook.firstMove();
-//        Rook rook2 = new Rook(1, 1, Piece.WHITE_PIECE);
-//        assertNotEquals(rook, rook2);
-//    }
-//
-//    @Test
-//    void bothRookMoved(){
-//        Rook rook = new Rook(1, 1, Piece.WHITE_PIECE);
-//        rook.firstMove();
-//        Rook rook2 = new Rook(1, 1, Piece.WHITE_PIECE);
-//        rook2.firstMove();
-//        assertEquals(rook, rook2);
-//    }
-//}
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class RookTest {
+    @Test
+    void rookHasntMoved(){
+        Chessboard board = new Chessboard();
+        Rook rook = new Rook(1, 1, Piece.WHITE_PIECE, board);
+        Rook rook2 = new Rook(1, 1, Piece.WHITE_PIECE, board);
+        assertEquals(rook, rook2);
+    }
+
+    @Test
+    void oneRookMoved(){
+        Chessboard board = new Chessboard();
+        Rook rook = new Rook(1, 1, Piece.WHITE_PIECE, board);
+        rook.firstMove();
+        Rook rook2 = new Rook(1, 1, Piece.WHITE_PIECE, board);
+        assertNotEquals(rook, rook2);
+    }
+
+    @Test
+    void bothRookMoved(){
+        Chessboard board = new Chessboard();
+        Rook rook = new Rook(1, 1, Piece.WHITE_PIECE, board);
+        rook.firstMove();
+        Rook rook2 = new Rook(1, 1, Piece.WHITE_PIECE, board);
+        rook2.firstMove();
+        assertEquals(rook, rook2);
+    }
+}
