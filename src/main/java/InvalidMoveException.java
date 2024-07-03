c5ee628f94d450a67de639e30eeb11011be58511
Toby <tobyeclark@gmail.com>
2024-07-03 12:58:31 +0100

    InvalidmoveExecption speicfies the coordinates tried

--- src/main/java/InvalidMoveException.java
+++ src/main/java/InvalidMoveException.java
@@ -1,5 +1,5 @@
 public class InvalidMoveException extends Exception {
-    public InvalidMoveException(String message) {
-        super(message);
+    public InvalidMoveException(int oldX, int oldY, int newX, int newY) {
+        super("Cannot move from " + new Coordinate(oldX, oldY) + " to " + new Coordinate(newX, newY));
     }
 }
