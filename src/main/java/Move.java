54f4d0880f0f9cd29243dd8e42df62c7a03aabb7
Toby <tobyeclark@gmail.com>
2024-07-03 13:49:52 +0100

    Added a method to MoveValue that checks if a piece is already in that positoin

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -58,7 +58,7 @@ public class Move {
     }
 
     private void takePiece(@NotNull MoveValue move){
-        if(move.newX() == move.piece().getX() && move.newY() == move.piece().getY()) // promotion
+        if(move.isPieceInSamePosition()) // promotion
             board.addPiece(move.piece());
         else
             taking = true;
@@ -85,13 +85,13 @@ public class Move {
 
     /** Checks if a piece was taken or if it was a promotion and restores it. */
     private void addOrRemovePiece(@NotNull Piece pieceToMove, @NotNull MoveValue move){
-        if(pieceToMove.getX() != move.newX() || pieceToMove.getY() != move.newY())
+        if(!move.isPieceInSamePosition())
             return;
         // a piece moving to the same spot only occurs as the last move when it's a promotion
         if(move == movesToUndo.getLast())
             board.removePiece(pieceToMove);
         else
-            board.addPiece(move.piece());
+            board.addPiece(pieceToMove);
     }
 
     public int getX() {return x;}
--- src/main/java/MoveValue.java
+++ src/main/java/MoveValue.java
@@ -3,6 +3,9 @@ import org.jetbrains.annotations.NotNull;
 
 /** A move a piece could make. */
 public record MoveValue(Piece piece, int newX, int newY) {
+    public boolean isPieceInSamePosition(){
+        return piece.getX() == newX && piece.getY() == newY;
+    }
     /**
      * Creates a move value which stays in the same position.
      * @param piece the piece which will be 'moved'
