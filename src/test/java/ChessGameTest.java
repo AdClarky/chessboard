bf3c5d3dfdb76ecb2cf3bcaa293a81a0b9f650c5
Toby <tobyeclark@gmail.com>
2024-07-05 16:44:56 +0100

    Added some tests for board changed events

--- src/test/java/ChessGameTest.java
+++ src/test/java/ChessGameTest.java
@@ -6,13 +6,14 @@ import java.nio.file.Path;
 
 class ChessGameTest {
     private static class Listener implements BoardListener {
-        int boardChanged = 0;
+        int movesMade = 0;
         int checkmate = 0;
         int draw = 0;
+        int boardChange = 0;
 
         @Override
         public void moveMade(int oldX, int oldY, int newX, int newY) {
-            boardChanged++;
+            movesMade++;
         }
 
         @Override
@@ -27,7 +28,7 @@ class ChessGameTest {
 
         @Override
         public void boardChanged(int oldX, int oldY, int newX, int newY) {
-
+            boardChange++;
         }
     }
     private Listener listener;
@@ -43,33 +44,95 @@ class ChessGameTest {
     @Test
     void basicMove(){
         assertDoesNotThrow(()-> chessGame.makeMove(3, 1, 3, 3));
-        assertEquals(1, listener.boardChanged);
+        assertEquals(1, listener.movesMade);
         assertEquals(0, listener.checkmate);
         assertEquals(0, listener.draw);
+        assertEquals(0, listener.boardChange);
     }
 
     @Test
     void checkmateGame() {
         assertDoesNotThrow( ()->new Autoplay(chessGame, Path.of("src/test/resources/checkmateGame.pgn")).play());
-        assertEquals(46, listener.boardChanged);
+        assertEquals(46, listener.movesMade);
         assertEquals(1, listener.checkmate);
         assertEquals(0, listener.draw);
+        assertEquals(0, listener.boardChange);
     }
 
     @Test
     void repetitionGame() {
         assertDoesNotThrow( ()->new Autoplay(chessGame, Path.of("src/test/resources/repetitionGame.pgn")).play());
-        assertEquals(8, listener.boardChanged);
+        assertEquals(8, listener.movesMade);
         assertEquals(0, listener.checkmate);
         assertEquals(1, listener.draw);
+        assertEquals(0, listener.boardChange);
     }
 
     @Test
     void stalemateGame() {
         assertDoesNotThrow( ()->new Autoplay(chessGame, Path.of("src/test/resources/stalemateGame.pgn")).play());
-        assertEquals(105, listener.boardChanged);
+        assertEquals(105, listener.movesMade);
         assertEquals(0, listener.checkmate);
         assertEquals(1, listener.draw);
+        assertEquals(0, listener.boardChange);
+    }
+
+    @Test
+    void oneMoveOneUndo(){
+        assertDoesNotThrow( ()->chessGame.makeMove(3, 1, 3, 3));
+        chessGame.undoMove();
+        assertEquals(1, listener.movesMade);
+        assertEquals(0, listener.checkmate);
+        assertEquals(0, listener.draw);
+        assertEquals(1, listener.boardChange);
+    }
+
+    @Test
+    void oneMoveUndoAndRedo(){
+        assertDoesNotThrow( ()->chessGame.makeMove(3, 1, 3, 3));
+        chessGame.undoMove();
+        chessGame.redoMove();
+        assertEquals(1, listener.movesMade);
+        assertEquals(0, listener.checkmate);
+        assertEquals(0, listener.draw);
+        assertEquals(2, listener.boardChange);
+    }
+
+    @Test
+    void fullGameUndoAllThenRedo() {
+        assertDoesNotThrow( ()->new Autoplay(chessGame, Path.of("src/test/resources/checkmateGame.pgn")).play());
+        chessGame.undoMultipleMoves(46);
+        chessGame.redoAllMoves();
+        assertEquals(46, listener.movesMade);
+        assertEquals(2, listener.checkmate);
+        assertEquals(0, listener.draw);
+        assertEquals(92, listener.boardChange);
+    }
+
+    @Test
+    void basicMoveThenRedoManyTimes(){
+        assertDoesNotThrow( ()->chessGame.makeMove(3, 1, 3, 3));
+        chessGame.undoMove();
+        chessGame.redoMove();
+        chessGame.redoMove();
+        chessGame.redoMove();
+        assertEquals(1, listener.movesMade);
+        assertEquals(0, listener.checkmate);
+        assertEquals(0, listener.draw);
+        assertEquals(2, listener.boardChange);
+    }
+
+    @Test
+    void basicMoveThenUndoManyTimes(){
+        assertDoesNotThrow( ()->chessGame.makeMove(3, 1, 3, 3));
+        chessGame.undoMove();
+        chessGame.undoMove();
+        chessGame.undoMove();
+        chessGame.undoMove();
+        assertEquals(1, listener.movesMade);
+        assertEquals(0, listener.checkmate);
+        assertEquals(0, listener.draw);
+        assertEquals(1, listener.boardChange);
     }
 
 
