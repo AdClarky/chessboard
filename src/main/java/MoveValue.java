ba04d949488faf25ec03920fc113742ed30bc5e7
Toby <tobyeclark@gmail.com>
2024-07-10 00:14:47 +0100

    Added docs to MoveVlaue

--- src/main/java/MoveValue.java
+++ src/main/java/MoveValue.java
@@ -1,11 +1,18 @@
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-/** A move a piece could make. */
+/**
+ * A move a piece could make.
+ */
 public record MoveValue(Piece piece, int newX, int newY) {
+    /**
+     * Checks if the Piece is in the same position as its new x and y.
+     * @return if the piece is in the same position.
+     */
     public boolean isPieceInSamePosition(){
         return piece.getX() == newX && piece.getY() == newY;
     }
+    
     /**
      * Creates a move value which stays in the same position.
      * @param piece the piece which will be 'moved'
