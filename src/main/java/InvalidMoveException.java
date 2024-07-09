0c209a6cf3d0127f1ebc0bab2d90b74aaefe0503
Toby <tobyeclark@gmail.com>
2024-07-10 00:12:49 +0100

    Added docs to invlaid move

--- src/main/java/InvalidMoveException.java
+++ src/main/java/InvalidMoveException.java
@@ -1,8 +1,24 @@
+/**
+ * Indicates a move is not a valid move given the current board state.
+ */
 public class InvalidMoveException extends Exception {
+    /**
+     * Creates an {@code InvalidMoveException} giving the message in
+     * algebraic form.
+     * @param oldX old x of piece
+     * @param oldY old y of piece
+     * @param newX new x of piece
+     * @param newY new y of piece
+     */
     public InvalidMoveException(int oldX, int oldY, int newX, int newY) {
         super("Cannot move from " + new Coordinate(oldX, oldY) + " to " + new Coordinate(newX, newY));
     }
-    public InvalidMoveException(String message) {
-        super("Cannot make move " + message);
+
+    /**
+     * Creates an {@code InvalidMoveException} giving the move given.
+     * @param move the move which was invalid.
+     */
+    public InvalidMoveException(String move) {
+        super("Cannot make move: " + move);
     }
 }
