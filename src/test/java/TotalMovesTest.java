e602c6422b8dc91d4acb65e49ea4cc2a0c289a7f
Toby <tobyclarkpub@gmail.com>
2024-07-16 22:08:22 +0100

    Added some tests for specific moves

--- src/test/java/TotalMovesTest.java
+++ src/test/java/TotalMovesTest.java
@@ -19,9 +19,22 @@ class TotalMovesTest {
         ChessGame board = new ChessGame();
         DepthTester tester = new DepthTester(board);
         assertEquals(8902, assertDoesNotThrow(()->tester.testDepth(3)));
-        assertEquals(34, tester.getCaptures());
-        assertEquals(12, tester.getChecks());
     }
+
+    @Test
+    void depth2a3() {
+        ChessGame board = assertDoesNotThrow(() -> new ChessGame("rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR b KQkq - 0 1"));
+        DepthTester tester = new DepthTester(board);
+        assertEquals(380, assertDoesNotThrow(()->tester.testDepth(2)));
+    }
+
+    @Test
+    void depth2a3a6() {
+        ChessGame board = assertDoesNotThrow(() -> new ChessGame("rnbqkbnr/1ppppppp/p7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 0 2"));
+        DepthTester tester = new DepthTester(board);
+        assertEquals(19, assertDoesNotThrow(()->tester.testDepth(1)));
+    }
+
     @Test
     void depth4(){
         ChessGame board = new ChessGame();
