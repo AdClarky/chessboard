5276eca21e24948b6b1441deedaac7d5dd9760a1
Toby <tobyclarkpub@gmail.com>
2024-07-16 21:15:38 +0100

    Moved depth tester into its own class

---
+++ src/test/java/DepthTester.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class DepthTester {
+    private final ChessGame board;
+    private int positions = 0;
+    private int captures = 0;
+    private int checks = 0;
+
+    public DepthTester(ChessGame board) {
+        this.board = board;
+    }
+
+    public void testDepth(int currentDepth) throws InvalidMoveException {
+        Collection<Piece> pieces = board.getColourPieces(board.getCurrentTurn());
+        for(Piece piece : pieces){
+            Collection<Coordinate> positionCoordinates = new ArrayList<>(piece.getPossibleMoves());
+            if(currentDepth == 1) {
+                positions += positionCoordinates.size();
+                continue;
+            }
+            for(Coordinate position : positionCoordinates){
+                Coordinate originalPos = new Coordinate(piece.getX(), piece.getY());
+                board.makeMove(piece.getX(), piece.getY(), position.x(), position.y());
+                if(board.wasMoveCapture())
+                    captures++;
+                if(board.isInCheck())
+                    checks++;
+                testDepth(currentDepth - 1);
+                board.undoMove();
+            }
+        }
+    }
+
+    public int getPositions() {
+        return positions;
+    }
+
+    public int getCaptures() {
+        return captures;
+    }
+
+    public int getChecks() {
+        return checks;
+    }
+}
