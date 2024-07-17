964994cfaed03efa3135b411a0c73806c89bcf79
Toby <tobyclarkpub@gmail.com>
2024-07-17 14:27:06 +0100

    Added tests no implementation for bitboard

---
+++ src/test/java/BitboardTest.java
@@ -0,0 +1,340 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class BitboardTest {
+
+    @Test
+    void addBasic(){
+
+    }
+
+    @Test
+    void addOutOfRange(){
+
+    }
+
+    @Test
+    void addNull(){
+
+    }
+
+
+    @Test
+    void removeNull() {
+
+    }
+
+    @Test
+    void removeEmpty(){
+
+    }
+
+    @Test
+    void removeNonEmpty(){
+
+    }
+
+    @Test
+    void removeNonCoordinate(){
+
+    }
+
+    @Test
+    void removeDoesNotContain(){
+
+    }
+
+    @Test
+    void removeCoordinate(){
+
+    }
+
+    @Test
+    void containsAllSameBitboard(){
+
+    }
+
+    @Test
+    void containsAllDifferentBitboard(){
+
+    }
+
+    @Test
+    void containsAllMainHasMoreThanOther(){
+
+    }
+
+    @Test
+    void containsAllButNullObj(){
+
+    }
+
+    @Test
+    void containsAllButEmptyObj(){
+
+    }
+
+    @Test
+    void containsAllValid(){
+
+    }
+
+    @Test
+    void containsAllCoordinateFalse(){
+
+    }
+
+    @Test
+    void addAllBitboard() {
+
+    }
+
+    @Test
+    void addAllSameBitboardFalse(){
+
+    }
+
+    @Test
+    void addAllNullInCollection() {
+
+    }
+
+    @Test
+    void addAllNotCoordinateCollection() {
+
+    }
+
+    @Test
+    void addAllAlreadyInCollection() {
+
+    }
+
+    @Test
+    void addAllEmptyCollection() {
+
+    }
+
+    @Test
+    void addAllNormal() {
+
+    }
+
+    @Test
+    void removeAllSameBitboard() {
+
+    }
+
+    @Test
+    void removeAllNothingInCommonBitboard() {
+
+    }
+
+    @Test
+    void removeAllSomeInCommonBitboard() {
+
+    }
+
+    @Test
+    void removeAllEmptyCollection() {
+
+    }
+
+    @Test
+    void removeAllNullInCollection() {
+
+    }
+
+    @Test
+    void removeAllSameCollection() {
+
+    }
+
+    @Test
+    void removeAllSomeInCommon() {
+
+    }
+
+    @Test
+    void retainAllSameBitboard() {
+
+    }
+
+    @Test
+    void retainAllCompletelyDifferentBitboard() {
+
+    }
+
+    @Test
+    void retainAllSimilarBitboard() {
+
+    }
+
+    @Test
+    void retainAllCollectionHasNull() {
+
+    }
+
+    @Test
+    void retainAllCollectionIsEmpty() {
+
+    }
+
+    @Test
+    void retainAllSameCollection() {
+
+    }
+
+    @Test
+    void retainAllSimilarCollection() {
+
+    }
+
+    @Test
+    void clearWhenEmpty() {
+
+    }
+
+    @Test
+    void clearWhenFull() {
+
+    }
+
+    @Test
+    void sameBitboardEquality() {
+
+    }
+
+    @Test
+    void differentBitboardEquality() {
+
+    }
+
+    @Test
+    void containsNull() {
+
+    }
+
+    @Test
+    void containsNonCoordinate() {
+
+    }
+
+    @Test
+    void containsButNotInCollection() {
+
+    }
+
+    @Test
+    void containsInCollection() {
+
+    }
+
+    @Test
+    void containsOutOfRange() {
+
+    }
+
+    @Test
+    void iteratorEmptyCollection() {
+
+    }
+
+    @Test
+    void iteratorFullCollection() {
+    }
+
+    @Test
+    void iteratorOnlyOneElement() {
+
+    }
+
+    @Test
+    void iteratorNextWhenNoElements() {
+
+    }
+
+    @Test
+    void iteratorNextWhenEmpty() {
+
+    }
+
+    @Test
+    void iteratorHasNextEmpty() {
+
+    }
+
+    @Test
+    void sizeOnEmpty() {
+    }
+
+    @Test
+    void sizeOnFull() {
+
+    }
+
+    @Test
+    void sizeOnPartiallyFull() {
+
+    }
+
+    @Test
+    void isEmptyOnFull() {
+
+    }
+
+    @Test
+    void isEmptyOnEmpty() {
+
+    }
+
+    @Test
+    void isEmptyOnPartiallyFull() {
+
+    }
+
+    @Test
+    void toArrayEmpty() {
+
+    }
+
+    @Test
+    void toArrayFull() {
+
+    }
+
+    @Test
+    void toArrayPartiallyFull() {
+
+    }
+
+    @Test
+    void toArrayCustomEmpty() {
+
+    }
+
+    @Test
+    void toArrayCustomFull() {
+
+    }
+
+    @Test
+    void toArrayCustomPartiallyFull() {
+
+    }
+
+    @Test
+    void toArrayCustomSizeSmaller() {
+
+    }
+
+    @Test
+    void toArrayCustomSizeBigger() {
+
+    }
+
+    @Test
+    void toArrayCustomSameSize() {
+
+    }
+}
