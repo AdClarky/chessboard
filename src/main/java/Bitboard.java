4197d9136fa50344c52d07b626eff1f7a9923fda
Toby <tobyclarkpub@gmail.com>
2024-07-17 12:24:49 +0100

    bitboard now implements Collections

--- src/main/java/Bitboard.java
+++ src/main/java/Bitboard.java
@@ -1,16 +1,51 @@
-public class Bitboard {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Bitboard implements Collection<Coordinate> {
     private long board = 0;
 
     public Bitboard() {
 
     }
 
-    public void add(Coordinate position){
+    @Override
+    public boolean add(Coordinate position) {
+        if(!position.isInRange())
+            throw new OutOfRangeException(position);
         board |= 1L << shift(position);
+        return true;
     }
 
-    public void remove(Coordinate position) {
-        board &= ~(1L << shift(position));
+    @Override
+    public boolean remove(Object o) {
+        if(!(o instanceof Coordinate coordinate))
+            throw new ClassCastException();
+        if(!contains(coordinate))
+            return false;
+        board &= ~(1L << shift(coordinate));
+        return true;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(@NotNull Collection<? extends Coordinate> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(@NotNull Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(@NotNull Collection<?> c) {
+        return false;
     }
 
     public void clear(){
@@ -21,8 +56,36 @@ public class Bitboard {
         return ((board >> shift(coordinate)) & 1) == 1;
     }
 
+    @Override
+    public int size() {
+        return Long.bitCount(board);
+    }
+
     public boolean isEmpty(){
         return board == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Coordinate> iterator() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @NotNull
+    @Override
+    public <T> T[] toArray(@NotNull T[] a) {
+        return null;
     }
 
     public long getBoard(){
