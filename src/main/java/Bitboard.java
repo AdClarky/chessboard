33303dcefd5419f4711394d7e9e0e81a173b086e
Toby <tobyclarkpub@gmail.com>
2024-07-17 13:49:38 +0100

    Implemented all methods in bitboard

--- src/main/java/Bitboard.java
+++ src/main/java/Bitboard.java
@@ -2,6 +2,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class Bitboard implements Collection<Coordinate> {
     private long board = 0;
@@ -12,6 +14,8 @@ public class Bitboard implements Collection<Coordinate> {
 
     @Override
     public boolean add(Coordinate position) {
+        if(position == null)
+            throw new NullPointerException();
         if(!position.isInRange())
             throw new OutOfRangeException(position);
         board |= 1L << shift(position);
@@ -20,6 +24,8 @@ public class Bitboard implements Collection<Coordinate> {
 
     @Override
     public boolean remove(Object o) {
+        if(o == null)
+            throw new NullPointerException();
         if(!(o instanceof Coordinate coordinate))
             throw new ClassCastException();
         if(!contains(coordinate))
@@ -35,7 +41,7 @@ public class Bitboard implements Collection<Coordinate> {
         for(Object o : c) {
             if (!(o instanceof Coordinate))
                 throw new ClassCastException();
-            if(!contains((Coordinate) o))
+            if(!contains(o))
                 return false;
         }
         return true;
@@ -43,25 +49,102 @@ public class Bitboard implements Collection<Coordinate> {
 
     @Override
     public boolean addAll(@NotNull Collection<? extends Coordinate> c) {
-        return false;
+        if(c instanceof Bitboard bitboard){
+            board |= bitboard.getBoard();
+            return true;
+        }
+        boolean changed = false;
+        for(Coordinate coordinate : c)
+            if(add(coordinate))
+                changed = true;
+        return changed;
     }
 
     @Override
     public boolean removeAll(@NotNull Collection<?> c) {
-        return false;
+        if(c instanceof Bitboard bitboard){
+            board &= ~bitboard.getBoard();
+            return true;
+        }
+        boolean changed = false;
+        for(Object o : c) {
+            if(remove(o))
+                 changed = true;
+        }
+        return changed;
     }
 
     @Override
     public boolean retainAll(@NotNull Collection<?> c) {
-        return false;
+        if(c instanceof Bitboard bitboard){
+            board = bitboard.getBoard() & board;
+            return true;
+        }
+        boolean changed = false;
+        Iterator<Coordinate> iterator = iterator();
+        while(iterator.hasNext()){
+            if(!c.contains(iterator.next())){
+                iterator.remove();
+                changed = true;
+            }
+        }
+        return changed;
     }
 
+    @Override
     public void clear(){
         board = 0;
     }
 
-    public boolean contains(Coordinate coordinate){
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Bitboard bitboard)) return false;
+        return board == bitboard.board;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(board);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if(o == null)
+            throw new NullPointerException();
+        if(!(o instanceof Coordinate coordinate))
+            throw new ClassCastException();
         return ((board >> shift(coordinate)) & 1) == 1;
+    }
+
+    private class Itr implements Iterator<Coordinate> {
+        int current = 63;
+        int numFound = 0;
+
+        Itr() {}
+
+        @Override
+        public boolean hasNext() {
+            return numFound != size();
+        }
+
+        @Override
+        public Coordinate next() {
+            if(current < 0 || numFound > size()){
+                throw new NoSuchElementException();
+            }
+            while(((board << current) & 1) == 0 && current >= 0){
+                current--;
+            }
+            numFound++;
+            return new Coordinate(getX(current), getY(current));
+        }
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Coordinate> iterator() {
+        return new Itr();
     }
 
     @Override
@@ -69,31 +152,38 @@ public class Bitboard implements Collection<Coordinate> {
         return Long.bitCount(board);
     }
 
+    @Override
     public boolean isEmpty(){
         return board == 0;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @NotNull
-    @Override
-    public Iterator<Coordinate> iterator() {
-        return null;
     }
 
     @NotNull
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] array = new Object[size()];
+        int i = 0;
+        for(int bit = 63; bit >= 0; bit--){
+            if(((board >> 63) & 1) == 0)
+                continue;
+            array[i++] = new Coordinate(getX(bit), getY(bit));
+        }
+        return array;
     }
 
     @NotNull
     @Override
+    @SuppressWarnings("unchecked")
     public <T> T[] toArray(@NotNull T[] a) {
-        return null;
+        if(a.length < size())
+            return (T[]) toArray();
+        int size = size();
+        Object[] array = toArray();
+        for(int i = 0; i< size; i++){
+            a[i] = (T) array[i];
+        }
+        if(a.length > size)
+            a[size] = null;
+        return a;
     }
 
     public long getBoard(){
@@ -102,5 +192,14 @@ public class Bitboard implements Collection<Coordinate> {
 
     private static int shift(Coordinate coordinate){
         return coordinate.x() + (coordinate.y() << 3);
+    }
+
+    private int getX(int bit){
+        int x = 8 - ((bit + 1) % 8);
+        return x == 8 ? 0 : x;
+    }
+
+    private int getY(int bit){
+        return 8 - Math.ceilDiv(bit + 1, 8);
     }
 }
