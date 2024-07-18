7438ddd179c5921da0086feb6266e6cddbc09d00
Toby <tobyclarkpub@gmail.com>
2024-07-18 17:05:37 +0100

    Added get piece method to pieceBoard

--- src/main/java/PieceBoard.java
+++ src/main/java/PieceBoard.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.Nullable;
+
 public class PieceBoard {
     private final Bitboard pawns = new Bitboard();
     private final Bitboard knights = new Bitboard();
@@ -8,33 +10,66 @@ public class PieceBoard {
 
     public PieceBoard(){}
 
-    public void add(Piece piece){
-        if(piece instanceof Pawn)
-            pawns.add(piece.getPosition());
-        else if(piece instanceof Knight)
-            knights.add(piece.getPosition());
-        else if(piece instanceof Rook)
-            rooks.add(piece.getPosition());
-        else if(piece instanceof Bishop)
-            bishops.add(piece.getPosition());
-        else if(piece instanceof Queen)
-            queens.add(piece.getPosition());
-        else if(piece instanceof King)
-            kings.add(piece.getPosition());
+    public void add(Pieces piece, Coordinate position){
+        switch(piece) {
+            case PAWN:
+                pawns.add(position);
+                break;
+            case KNIGHT:
+                knights.add(position);
+                break;
+            case ROOK:
+                rooks.add(position);
+                break;
+            case BISHOP:
+                bishops.add(position);
+                break;
+            case QUEEN:
+                queens.add(position);
+                break;
+            case KING:
+                kings.add(position);
+                break;
+        }
     }
 
-    public void remove(Piece piece){
-        if(piece instanceof Pawn)
-            pawns.remove(piece.getPosition());
-        else if(piece instanceof Knight)
-            knights.remove(piece.getPosition());
-        else if(piece instanceof Rook)
-            rooks.remove(piece.getPosition());
-        else if(piece instanceof Bishop)
-            bishops.remove(piece.getPosition());
-        else if(piece instanceof Queen)
-            queens.remove(piece.getPosition());
-        else if(piece instanceof King)
-            kings.remove(piece.getPosition());
+    public void remove(Pieces piece, Coordinate position){
+        switch(piece) {
+            case PAWN:
+                pawns.remove(position);
+                break;
+            case KNIGHT:
+                knights.remove(position);
+                break;
+            case ROOK:
+                rooks.remove(position);
+                break;
+            case BISHOP:
+                bishops.remove(position);
+                break;
+            case QUEEN:
+                queens.remove(position);
+                break;
+            case KING:
+                kings.remove(position);
+                break;
+        }
+    }
+
+    @Nullable
+    public Pieces getPiece(Coordinate position){
+        if(pawns.contains(position))
+            return Pieces.PAWN;
+        if(knights.contains(position))
+            return Pieces.KNIGHT;
+        if(bishops.contains(position))
+            return Pieces.BISHOP;
+        if(rooks.contains(position))
+            return Pieces.ROOK;
+        if(queens.contains(position))
+            return Pieces.QUEEN;
+        if(kings.contains(position))
+            return Pieces.KING;
+        return null;
     }
 }
