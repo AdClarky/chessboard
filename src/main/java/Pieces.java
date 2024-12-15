929bfeaca8d83c922049994e1aebd8ec698131b3
Toby <tobyeclark@gmail.com>
2024-12-15 13:10:18 +0000

    Pieces can now be converted to a char

--- src/main/java/Pieces.java
+++ src/main/java/Pieces.java
@@ -1,9 +1,19 @@
 public enum Pieces {
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    PAWN,
-    QUEEN,
-    KING,
-    BLANK;
+    ROOK('R'),
+    KNIGHT('N'),
+    BISHOP('B'),
+    PAWN('\u0000'),
+    QUEEN('Q'),
+    KING('K'),
+    BLANK('Z');
+
+    private final char character;
+
+    Pieces(char character) {
+        this.character = character;
+    }
+
+    public char toCharacter(){
+        return character;
+    }
 }
