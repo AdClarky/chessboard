bb1ac036f9597a7ae91c46c93b361aff86358ae8
Toby <tobyeclark@gmail.com>
2025-02-13 23:15:45 +0000

    Added ability to get a piece value from a coordinate and character

--- src/main/java/common/PieceValue.java
+++ src/main/java/common/PieceValue.java
@@ -1,4 +1,19 @@
 package common;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public record PieceValue(Coordinate position, Pieces pieceType, PieceColour colour) {
+    @Contract("_, _ -> new")
+    public static @NotNull PieceValue of(Coordinate position, Character piece) {
+        PieceColour colour;
+        if(Character.isLowerCase(piece)) {
+            colour = PieceColour.BLACK;
+        }else{
+            colour = PieceColour.WHITE;
+        }
+        Pieces type = Pieces.valueOf("" + Character.toUpperCase(piece));
+
+        return new PieceValue(position, type, colour);
+    }
 }
