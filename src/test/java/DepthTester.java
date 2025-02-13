8ddc9ab0305c9c6f295339d60706ef6debf5b08a
Toby <tobyeclark@gmail.com>
2025-02-13 18:54:18 +0000

    Removed print messages from Depth tester

--- src/test/java/DepthTester.java
+++ src/test/java/DepthTester.java
@@ -7,6 +7,7 @@ public class DepthTester {
     public DepthTester(ChessGame game, int topDepth) {
         this.game = game;
         this.topDepth = topDepth;
+        System.out.println(new String("Testing depth " + topDepth));
     }
 
     public int testDepth(int currentDepth) throws InvalidMoveException {
@@ -21,7 +22,7 @@ public class DepthTester {
             for(Coordinate newMove : positionCoordinates){
                 game.makeMove(piece, newMove);
                 int currentPos = testDepth(currentDepth - 1);
-                if(currentDepth == topDepth) System.out.println(new String("" + piece + newMove + ": " + currentPos));
+//                if(currentDepth == topDepth) System.out.println(new String("" + piece + newMove + ": " + currentPos));
                 positions += currentPos;
                 game.undoMove();
             }
